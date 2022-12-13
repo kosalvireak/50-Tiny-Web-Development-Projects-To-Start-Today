a3f9250765592f9863e8bf5ab10bf861b41f2340
Kosalvireak Khoeun <virakvary@gmail.com>
2022-12-14 00:57:46 +0700

    finish

--- 2. Progress Steps/script.js
+++ 2. Progress Steps/script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var health = document.getElementById("health");
-
 });
 
 function next() {
@@ -14,10 +13,33 @@ function pre() {
 };
 function check() {
 
+    var num2 = document.querySelectorAll("p")[1];
+    var num3 = document.querySelectorAll("p")[2];
+    var num4 = document.querySelectorAll("p")[3];
+    if (health.value >= 99.99) {
+        num2.classList.add('bor');
+        num3.classList.add('bor');
+        num4.classList.add('bor');
+
+    }
+    else if (health.value >= 66.66) {
+        num2.classList.add('bor');
+        num3.classList.add('bor');
+        num4.classList.remove('bor');
+    }
+    else if (health.value >= 33.33) {
+        num2.classList.add('bor');
+        num3.classList.remove('bor');
+    }
+    else {
+        num2.classList.remove('bor');
+    }
+
+
     if (health.value <= 0) {
         document.querySelector(".button_pre").disabled = true;
     }
-    else if (health.value >= 100) {
+    else if (health.value >= 99.99) {
         document.querySelector(".button_next").disabled = true;
     }
     else {
